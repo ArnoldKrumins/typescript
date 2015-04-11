6500694e8c7d06c225a4ccca2059c0991ed2588f
Arnold Krumins <arnold.krumins@gmail.com>
2015-04-11 20:02:17 +0200

    putting initialisation code in constructor

--- patterns/strategy.ts
+++ patterns/strategy.ts
@@ -7,56 +7,60 @@ interface taxable{
 
 class tax{
     value:number;
+    name:string;
     constructor(paramvalue: number) { this.value = paramvalue; }
 }
 
 
 class ukTax extends tax implements taxable{
 
-    constructor(value: number) { super(value); }
+    constructor(value: number){
+        this.name ='UK Tax';
+        super(value);
+    }
 
     calculate(){
-        return this.value * 2;
+        return this.name + ' - ' + this.value * 2;
     }
 }
 
 
 class usTax extends tax implements taxable{
 
-    constructor(value: number) { super(value); }
+    constructor(value: number) {
+        this.name ='US Tax';
+        super(value);
+    }
 
     calculate(){
-        return this.value * 2.5;
+        return this.name + ' - ' + this.value * 2.5;
     }
 }
 
 
 class seTax extends tax implements taxable{
 
-    constructor(value: number) { super(value); }
+    constructor(value: number) {
+        this.name ='SE Tax';
+        super(value);
+    }
 
     calculate(){
-        return this.value * 1.5;
+        return this.name + ' - ' + this.value * 1.5;
     }
 }
 
 
-
+//Create a 'taxProvier' class to access tax strategy objects
 class taxProvider{
 
    private dict: { [index: number]: tax; } = {};
 
-   public init(index:number, taxObject:tax){
-
-       this.dict[index] = taxObject;
-   }
-
-   public setup(){
-
-       this.dict[1] = new ukTax(10);
-       this.dict[2] = new usTax(10);
-       this.dict[3] = new seTax(10);
-   }
+    constructor(){
+        this.dict[1] = new ukTax(10);
+        this.dict[2] = new usTax(10);
+        this.dict[3] = new seTax(10);
+    }
 
    public get(index:number){
         return this.dict[index];
